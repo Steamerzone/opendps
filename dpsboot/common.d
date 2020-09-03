d2c2bfc5535e619b4d237fa1a6d030243fbfd795
Steamerzone <steamerzone@gmail.com>
2020-09-04 01:06:26 +0200

    Add files via upload

---
+++ dpsboot/common.d
@@ -0,0 +1,40 @@
+common.o: common.c ../opendps/protocol.c \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /usr/arm-none-eabi/include/string.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/sys/string.h ../opendps/protocol.h \
+ ../opendps/uframe.h ../opendps/dbg_printf.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h ../opendps/uframe.c \
+ ../opendps/crc16.h ../opendps/crc16.c ../opendps/bootcom.c \
+ ../opendps/bootcom.h ../opendps/flashlock.c \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/flash.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/common.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/flash.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/flash_common_f01.h \
+ ../opendps/flashlock.h ../opendps/past.c ../opendps/past.h \
+ ../opendps/ringbuf.c ../opendps/ringbuf.h ../opendps/tick.c \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/systick.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/nvic.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/nvic.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/nvic.h \
+ ../opendps/tick.h
---
+++ dpsboot/dpsboot.d
@@ -0,0 +1,54 @@
+dpsboot.o: dpsboot.c /usr/arm-none-eabi/include/stdio.h \
+ /usr/arm-none-eabi/include/_ansi.h /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdarg.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/types.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/arm-none-eabi/include/machine/types.h \
+ /usr/arm-none-eabi/include/sys/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdbool.h \
+ /usr/arm-none-eabi/include/string.h \
+ /usr/arm-none-eabi/include/sys/string.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/common.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/scb.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /usr/arm-none-eabi/include/stdlib.h \
+ /usr/arm-none-eabi/include/machine/stdlib.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/cm3/systick.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/usart.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/usart.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/usart_common_all.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/timer.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/timer.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/timer_common_all.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/flash.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/f1/flash.h \
+ /home/pi/proj/opendpscolorsamefont/libopencm3/include/libopencm3/stm32/common/flash_common_f01.h \
+ ../opendps/tick.h hw.h ../opendps/ringbuf.h ../opendps/past.h \
+ ../opendps/pastunits.h ../opendps/uframe.h ../opendps/dbg_printf.h \
+ ../opendps/protocol.h ../opendps/uframe.h ../opendps/bootcom.h \
+ ../opendps/crc16.h ../opendps/flashlock.h
